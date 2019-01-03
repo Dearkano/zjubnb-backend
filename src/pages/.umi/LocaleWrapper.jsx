b96624ebe6765293f364409820d1ddd060aea012
vaynetian <vaynetian@cc98.org>
2019-01-03 10:11:20 +0800

    update language

--- src/pages/.umi/LocaleWrapper.jsx
+++ src/pages/.umi/LocaleWrapper.jsx
@@ -16,8 +16,7 @@ const useLocalStorage = true;
 
 import { LocaleProvider } from 'antd';
 import moment from 'moment';
-import 'moment/locale/zh-cn';
-let defaultAntd = require('antd/lib/locale-provider/zh_CN');
+let defaultAntd = require('antd/lib/locale-provider/en_US');
 defaultAntd = defaultAntd.default || defaultAntd;
 
 const localeInfo = {
@@ -60,17 +59,17 @@ const localeInfo = {
 };
 
 let appLocale = {
-  locale: 'zh-CN',
+  locale: 'en-US',
   messages: {},
-  data: require('react-intl/locale-data/zh'),
-  momentLocale: 'zh-cn',
+  data: require('react-intl/locale-data/en'),
+  momentLocale: '',
 };
 if (useLocalStorage && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
   appLocale = localeInfo[localStorage.getItem('umi_locale')];
 } else if (localeInfo[navigator.language] && baseNavigator){
   appLocale = localeInfo[navigator.language];
 } else {
-  appLocale = localeInfo['zh-CN'] || appLocale;
+  appLocale = localeInfo['en-US'] || appLocale;
 }
 window.g_lang = appLocale.locale;
 appLocale.data && addLocaleData(appLocale.data);
